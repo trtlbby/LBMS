1b620786b152a440a305354b4ba8a7f0b2611796
trtlbby <earllawrence.bacsain@unc.edu.ph>
2025-01-31 20:07:25 +0800

    🚀Added Search function to Student List

--- src/main/java/com/example/testdb/StudentListViewController.java
+++ src/main/java/com/example/testdb/StudentListViewController.java
@@ -1,17 +1,19 @@
 package com.example.testdb;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+/**
+ * @Author: Earl Lawrence P. Bacsain
+ **/
 public class StudentListViewController {
     public TableView<Student> tblStudentList;
     public TableColumn<Student, String> colID;
@@ -77,5 +79,40 @@ public class StudentListViewController {
     }
 
     public void onClickSearch(ActionEvent actionEvent) {
+        tblStudentList.getColumns().clear();
+        tblStudentList.getItems().clear();
+
+        String searchKey = txtfieldSearch.getText().trim();
+        ObservableList<Student> studentObservableList = FXCollections.observableArrayList();
+
+        try {
+            Connection CONN = JDBCConnector.connection();
+            String sql = "SELECT * FROM student_attendance" +
+                    " WHERE CONCAT(stud_num, ' ', name, ' ', purpose, " +
+                    "' ', college_ID) LIKE '%"+searchKey+"%'";
+            PreparedStatement preparedStatement = CONN.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){
+                Student student = new Student(rs.getString("stud_num"),
+                        rs.getString("name"),
+                        rs.getString("purpose"),
+                        rs.getString("college_ID"));
+                studentObservableList.add(student);
+            }
+
+            colID.setCellValueFactory(new PropertyValueFactory<>("stud_num"));
+            colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+            colPurpose.setCellValueFactory(new PropertyValueFactory<>("purpose"));
+            colCollege.setCellValueFactory(new PropertyValueFactory<>("college_ID"));
+
+            tblStudentList.getColumns().addAll(colID, colName, colPurpose, colCollege);
+            tblStudentList.setItems(studentObservableList);
+
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Error is there\n" + e.getMessage());
+            alert.showAndWait();
+        }
     }
 }
